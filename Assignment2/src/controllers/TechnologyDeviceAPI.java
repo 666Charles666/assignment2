c5dedde3ea5f076bc1c03de98fc6490aa043bae9
ZhangRongchang <1024485669@qq.com>
2024-05-19 00:53:12 +0800

    add a new class

--- Assignment2/src/controllers/TechnologyDeviceAPI.java
+++ Assignment2/src/controllers/TechnologyDeviceAPI.java
@@ -9,42 +9,126 @@ import utils.Utilities;
 
 import java.io.*;
 import java.util.*;
+import controllers.ManufacturerAPI;
 
 
 
 //TODO - ensure that this class implements iSerializer
 public class TechnologyDeviceAPI implements ISerializer{
     //TODO - create 2 fields
+    private List<Technology> technologyList;
+    private File file;
+
 
     //TODO - create constructor
-   //TODO - CRUD Methods
+    public TechnologyDeviceAPI(){
+        this.technologyList = new ArrayList<>();
+    }
 
 
+
+    //TODO - CRUD Methods
+     public boolean addTechnologyDevice(Technology technology){
+        return technologyList.add(technology);
+     }
+    public Technology deleteTechnologyByIndex(int index){
+        if (isValidIndex(index)){
+            return technologyList.remove(index - 1);
+        }else return null;
+    }
 
 
     //TODO - Number methods
-
+    public int numberOfTechnologyByChosenManufacture(String manufacturerName){
+        int count = 0;
+        for (Technology technology : technologyList){
+            if (manufacturerName.equals(technology.getModelName())){
+                count++;
+            }
+        }
+        return count;
+    }
 
     // TODO Read/list methods
-
+    public String listAllTechnologyDevices(){
+        String tec = "";
+        if (technologyList.size() != 0){
+            for (Technology technology : technologyList){
+                tec += technologyList.indexOf(technology) + ": " + technology + "\n";
+            }
+            return tec;
+        }return "No Technology Devices";
+    }
+    public String listAllSmartBands(){
+        String tec = "";
+        for (Technology technology : technologyList){
+            if (technology instanceof SmartBand){
+                tec += technologyList.indexOf(technology) + ": " + technology + "\n";
+            }return tec;
+        }return "“No Smart Bands";
+    }
+    public String listAllSmartWatches(){
+        String tec = "";
+        for (Technology technology : technologyList){
+            if (technology instanceof SmartWatch){
+                tec += technologyList.indexOf(technology) + ": " + technology + "\n";
+            }return tec;
+        }return "No Smart Watches";
+    }
+    public String listAllTablets(){
+        String tec = "";
+        for (Technology technology : technologyList){
+            if (technology instanceof Tablet){
+                tec += technologyList.indexOf(technology) + ": " + technology + "\n";
+            }return tec;
+        }return "No Tablet";
+    }
 
 
     //the following is isValidId can be updated
     //to suit your code
-    /*public boolean isValidId(String id) {
+     public boolean isValidId(String id) {
         for (Technology techDev : technologyList) {
             if (techDev.getId().equals(id))
                 return false;
         }
             return true;
         }
-*/
+    public boolean isValidIndex(int index){
+        if (index > 0 && index <= technologyList.size() +1 ){
+            return true;
+        }else return false;
+    }
+
 
     //TODO get Technology methods
+    public Technology getTechnologyByIndex(int index){
+        if (isValidIndex(index)){
+            return technologyList.get(index - 1);
+        }else return null;
+    }
+    private Technology getTechnologyDeviceById(String id){
+        int count = 0;
+        for (Technology technology : technologyList){
+            if (id.equals(technology.getId())){
+                return technologyList.get(count);
+            }else count++;
+        }return null;
+    }
 
    //TODO - delete methods
 
-
+    public Technology deleteTechnologyById(String id){
+        int count = 0;
+        if (isValidId(id)){
+            for (Technology technology : technologyList){
+                if (id.equals(technology.getId())){
+                    technologyList.remove(count);
+                    return technologyList.get(count);
+                }else count++;
+            }return technologyList.get(count);
+        }else return null;
+    }
 
     //TODO - sort methods
 
